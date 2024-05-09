a199eb24a3dc5bd7dec08ac085e8448a554cce5f
LILIAN FLORES <18170322@itculiacan.edu.mx>
2024-05-08 19:27:50 -0700

    crear un elemento y agregarlo a la list

--- src/index.js
+++ src/index.js
@@ -18,4 +18,14 @@ app.get('/Libros', async (req, res) => {
     }
 })
 
+//creacion de elementos
+app.post('/Libros', async (req, res) => {
+    try {
+        const {ID,Titulo,Autor,Editorial, ISBN, Fecha_de_publicacion} = req.body;
+        const [rows] = await pool.query('INSERT INTO Libros (ID,Titulo,Autor,Editorial, ISBN, Fecha_de_publicacion) VALUES (?,?,?,?,?,?)', [ID,Titulo,Autor,Editorial, ISBN, Fecha_de_publicacion])
+        res.status(201).json({Titulo})
+    } catch(error) {
+        return res.status(500).json({message: 'Error al realizar la consulta'})
+    }
+})
 app.listen(2000)
