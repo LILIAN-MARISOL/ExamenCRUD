b4e9a6b944c3742c7ed4d1040a9db89728a9f6c1
LILIAN FLORES <18170322@itculiacan.edu.mx>
2024-05-08 22:39:36 -0700

    corrección put and delete

--- src/index.js
+++ src/index.js
@@ -41,18 +41,34 @@ app.get('/Libros/:ID', async (req, res) => {
 })
 
 //d. En detalles o en otra pantalla pueda editar y guardar los cambios de este elemento
-app.put('/Libros/:ID', async (req, res) => {
+app.put('/Libros', async (req, res) => {
     try {
-        const {id} = req.params
         const {ID,Titulo,Autor,Editorial, ISBN, Fecha_de_publicacion} = req.body;
-        const [resultado] = await pool.query("UPDATE Libros SET ID = IFNULL(?, ID), Titulo = IFNULL(?, Titulo), Autor = IFNULL(?, Autor), Editorial = IFNULL(?, Editorial), ISBN = IFNULL(?, ISBN), Fecha_de_publicacion = IFNULL(?, Fecha_de_publicacion) WHERE ID = ?", [Titulo,Autor,Editorial, ISBN, Fecha_de_publicacion,ID])
+        const [resultado] = await pool.query(`UPDATE Libros SET Titulo = "${Titulo}", Autor = "${Autor}", Editorial = "${Editorial}", ISBN = "${ISBN}", Fecha_de_publicacion = "${Fecha_de_publicacion}" WHERE ID = ${ID}`);
 
         if (resultado.affectedRows === 0) return res.status(404).json({message: "No se encontro el libro"})
 
-        const [updated] = await pool.query("SELECT * FROM Libros WHERE ID = ?", [ID])
+        const [updated] = await pool.query("SELECT * FROM Libros WHERE ID = ?", ID)
         res.json(updated);
     } catch (error) {
+        console.log(error)
         return res.status(500).json({message: "Error al realizar la consulta."})
+    }
+})
+//e. Pueda borrar el elemento en cuestión.
+app.delete('/Libros/:ID', async (req, res) => {
+    try {
+        const {ID} = req.params
+        console.log(ID)
+        const [rows] = await pool.query('DELETE FROM Libros WHERE ID = ?', ID)
+
+        if (rows.affectedRows <= 0) {
+            return res.status(404).json({message: 'No se encontro el libro'})
+        }
+
+        res.send({message: 'Se elimino el libro'})
+    } catch (error) {
+
     }
 })
 
